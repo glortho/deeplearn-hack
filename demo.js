244c1cb04f6b1d7562701b4cc01e35090a59318f
glortho <jed.verity@digitalglobe.com>
2017-10-18 12:06:09 -0600

    NDArrayMathGPU test

--- demo.js
+++ demo.js
@@ -1,12 +1,18 @@
-import {Array1D, NDArrayMathGPU, Scalar} from 'deeplearn';
+import {Array2D, NDArrayMathGPU, Scalar} from 'deeplearn';
 
 export default () => {
   const math = new NDArrayMathGPU();
-  const a = Array1D.new([1, 2, 3]);
-  const b = Scalar.new(2);
 
-  const result = math.add(a, b);
-  // Float32Array([3, 4, 5])
-  result.getValuesAsync().then(values => console.log(values));
+  const a = Array2D.new([2, 2], [1.0, 2.0, 3.0, 4.0]);
+  const b = Array2D.new([2, 2], [0.0, 2.0, 4.0, 6.0]);
+
+  // Non-blocking math calls.
+  const diff = math.sub(a, b);
+  const squaredDiff = math.elementWiseMul(diff, diff);
+  const sum = math.sum(squaredDiff);
+  const size = Scalar.new(a.size);
+  const average = math.divide(sum, size);
+
+  console.log('mean squared difference: ' + average.get());
 }
 
